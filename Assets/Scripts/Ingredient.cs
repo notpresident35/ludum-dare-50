462bec20f640041829f62391b019bed0d3e47418
notpresident35 <notpresident35@gmail.com>
2022-04-02 16:44:13 -0700

    Started Systems
    Created Alchemy System, some placeholder scripts and folders, and main scene

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName="Ingredient")]
+public class Ingredient : ScriptableObject
+{
+    public char Abbreviation;
+    public GameObject FallingPrefab;
+}
