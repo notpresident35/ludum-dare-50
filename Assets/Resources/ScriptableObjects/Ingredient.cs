254be81247ce3d94ec60f5f96619f1dd87ffda28
notpresident35 <notpresident35@gmail.com>
2022-04-03 10:06:42 -0700

    Falling Ingredients

--- Assets/Resources/ScriptableObjects/Ingredient.cs
+++ Assets/Resources/ScriptableObjects/Ingredient.cs
@@ -2,12 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName="Ingredient")]
+[CreateAssetMenu(menuName = "Ingredient")]
 public class Ingredient : ScriptableObject
 {
-    public char Abbreviation;
-    public int Frequency;
-    [Range(0, 4)]
-    public float GravityModifier;
-    public GameObject FallingPrefab;
+	public char Abbreviation;
+	public int Frequency;
+	[Range(0, 5)]
+	public float GravityModifier;
+	public GameObject FallingPrefab;
 }
