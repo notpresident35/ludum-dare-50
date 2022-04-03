22b40d2e0ca5253930fba9caad61484e1d7a176b
notpresident35 <notpresident35@gmail.com>
2022-04-03 13:36:41 -0700

    Added debug and error-handling

--- Assets/Scripts/Campaign/CampaignManager.cs
+++ Assets/Scripts/Campaign/CampaignManager.cs
@@ -27,19 +27,31 @@ namespace GameJam
 
 		public void SpawnHero(int trackID, Hero hero)
 		{
+			if (trackID == -1)
+			{
+				Debug.LogError("Tried to spawn hero with invalid track id!");
+				return;
+			}
 			HeroInstance newHero = Instantiate(HeroPrefab, Tracks[trackID].HeroSpawnpoint.position, Quaternion.identity, Tracks[trackID].transform).GetComponent<HeroInstance>();
 			newHero.entity = hero;
 			newHero.Spawn();
 			Tracks[trackID].AttachHero(newHero);
+			print("Spawned Hero: " + newHero.name);
 		}
 
 		public void SpawnMonster(int trackID, Monster monster)
 		{
+			if (trackID == -1)
+			{
+				Debug.LogError("Tried to spawn monster with invalid track id!");
+				return;
+			}
 			MonsterInstance newMonster = Instantiate(MonsterPrefab, Tracks[trackID].MonsterSpawnpoint.position, Quaternion.identity, Tracks[trackID].transform).GetComponent<MonsterInstance>();
 			newMonster.transform.parent = Tracks[trackID].transform;
 			newMonster.entity = monster;
 			newMonster.Spawn();
 			Tracks[trackID].AttachMonster(newMonster);
+			print("Spawned Monster: " + newMonster.name);
 		}
 	}
 }
