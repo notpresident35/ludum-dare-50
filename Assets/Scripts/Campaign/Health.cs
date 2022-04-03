b4eef530c6e2430b20144192730b8caa5387a3ea
notpresident35 <notpresident35@gmail.com>
2022-04-02 22:02:33 -0700

    Campaign system complete

--- Assets/Scripts/Campaign/Health.cs
+++ Assets/Scripts/Campaign/Health.cs
@@ -5,17 +5,18 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     
-    public float Value;
+    public float health;
     
     public void Damage (float amount) {
-        Value -= amount;
-        if (Value <= 0) {
+        print (gameObject.name + " was attacked");
+        health -= amount;
+        if (health <= 0) {
             Die ();
         }
     }
 
     public void Heal (float amount) {
-        Value += amount;
+        health += amount;
     }
 
     public virtual void Die() {
