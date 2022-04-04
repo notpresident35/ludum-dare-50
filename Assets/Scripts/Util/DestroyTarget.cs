901eccf93cb68366f7efc3bc6c474a61bf70a91a
Anguillifax <anguillifax@gmail.com>
2022-04-03 20:53:12 -0700

    Add Bucket Movement, Update VFX

---
+++ Assets/Scripts/Util/DestroyTarget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam
+{
+	public class DestroyTarget : MonoBehaviour
+	{
+		public UnityEngine.Object target;
+
+		public void Execute()
+		{
+			Destroy(target);
+		}
+	}
+}
